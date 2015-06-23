e575b1c03c46216a6929475e10142a20827eebac
Sebastián Mancilla <smancill@jlab.org>
2015-06-23 13:55:53 -0300

    xMsgException: update Javadoc

--- src/org/jlab/coda/xmsg/excp/xMsgException.java
+++ src/org/jlab/coda/xmsg/excp/xMsgException.java
@@ -29,6 +29,14 @@ package org.jlab.coda.xmsg.excp;
  */
 public class xMsgException extends Exception {
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param   message the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
     public xMsgException(String message) {
         super(message);
     }
