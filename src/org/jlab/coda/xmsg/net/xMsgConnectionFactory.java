f4d9ef13b36bc5720cc7ecbcd6afd5e88982c831
Sebastián Mancilla <smancill@jlab.org>
2016-01-14 18:47:45 -0300

    Connection: add new control socket
    Use this socket to synchronize connection and subscription.

--- src/org/jlab/coda/xmsg/net/xMsgConnectionFactory.java
+++ src/org/jlab/coda/xmsg/net/xMsgConnectionFactory.java
@@ -1,5 +1,10 @@
 package org.jlab.coda.xmsg.net;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Random;
+
+import org.jlab.coda.xmsg.core.xMsgUtil;
 import org.jlab.coda.xmsg.xsys.regdis.xMsgRegDriver;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
@@ -20,19 +25,29 @@ public class xMsgConnectionFactory {
                                                 xMsgConnectionSetup setup) {
         Socket pubSock = context.createSocket(ZMQ.PUB);
         Socket subSock = context.createSocket(ZMQ.SUB);
+        Socket ctrlSock = context.createSocket(ZMQ.DEALER);
+
         setup.preConnection(pubSock);
         setup.preConnection(subSock);
 
         int pubPort = address.port();
         int subPort = pubPort + 1;
+        int ctrlPort = subPort + 1;
+
+        String identity = getCtrlId();
+        ctrlSock.setIdentity(identity.getBytes());
+
         pubSock.connect("tcp://" + address.host() + ":" + pubPort);
         subSock.connect("tcp://" + address.host() + ":" + subPort);
+        ctrlSock.connect("tcp://" + address.host() + ":" + ctrlPort);
         setup.postConnection();
 
         xMsgConnection connection = new xMsgConnection();
         connection.setAddress(address);
         connection.setPubSock(pubSock);
         connection.setSubSock(subSock);
+        connection.setControlSock(ctrlSock);
+        connection.setIdentity(identity);
 
         return connection;
     }
@@ -59,5 +74,25 @@ public class xMsgConnectionFactory {
 
     public void destroy() {
         context.destroy();
+    }
+
+
+    // CHECKSTYLE.OFF: ConstantName
+    private static final Random randomGenerator = new Random();
+    private static final long ctrlIdPrefix = getCtrlIdPrefix();
+    // CHECKSTYLE.ON: ConstantName
+
+    private static long getCtrlIdPrefix() {
+        try {
+            final int javaId = 1;
+            final int ipHash = Math.abs(xMsgUtil.localhost().hashCode());
+            return (ipHash % 1000) * 1000000 + javaId * 100000;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    static String getCtrlId() {
+        return Long.toString(ctrlIdPrefix + randomGenerator.nextInt(100000));
     }
 }
---
+++ test/org/jlab/coda/xmsg/net/xMsgConnectionFactoryTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015. Jefferson Lab, xMsg framework (JLAB). All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for educational, research, and not-for-profit purposes,
+ * without fee and without a signed licensing agreement.
+ *
+ * Contact Vardan Gyurjyan
+ * Department of Experimental Nuclear Physics, Jefferson Lab.
+ *
+ * IN NO EVENT SHALL JLAB BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF JLAB HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * JLAB SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE CLARA SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". JLAB HAS NO OBLIGATION TO PROVIDE MAINTENANCE,
+ * SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+package org.jlab.coda.xmsg.net;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class xMsgConnectionFactoryTest {
+
+    @Test
+    public void ctrlIdentityHas9Digits() throws Exception {
+        assertThat(xMsgConnectionFactory.getCtrlId().length(), is(9));
+        assertThat(xMsgConnectionFactory.getCtrlId().length(), is(9));
+        assertThat(xMsgConnectionFactory.getCtrlId().length(), is(9));
+    }
+
+    @Test
+    public void ctrlIdentityPrefixHas3Digits() throws Exception {
+        String prefix1 = xMsgConnectionFactory.getCtrlId().substring(0, 3);
+        String prefix2 = xMsgConnectionFactory.getCtrlId().substring(0, 3);
+        String prefix3 = xMsgConnectionFactory.getCtrlId().substring(0, 3);
+
+        assertThat(prefix1, is(prefix2));
+        assertThat(prefix1, is(prefix3));
+        assertThat(prefix2, is(prefix3));
+    }
+
+    @Test
+    public void ctrlIdentityFourthDigitIsJavaIdentifier() throws Exception {
+        assertThat(xMsgConnectionFactory.getCtrlId().charAt(3), is('1'));
+        assertThat(xMsgConnectionFactory.getCtrlId().charAt(3), is('1'));
+        assertThat(xMsgConnectionFactory.getCtrlId().charAt(3), is('1'));
+    }
+
+    @Test
+    public void ctrlIdentityPrefixLastFiveDigitsAreRandom() throws Exception {
+        String suffix1 = xMsgConnectionFactory.getCtrlId().substring(4);
+        String suffix2 = xMsgConnectionFactory.getCtrlId().substring(4);
+        String suffix3 = xMsgConnectionFactory.getCtrlId().substring(4);
+
+        assertThat(suffix1, is(not(suffix2)));
+        assertThat(suffix1, is(not(suffix3)));
+        assertThat(suffix2, is(not(suffix3)));
+    }
+}
