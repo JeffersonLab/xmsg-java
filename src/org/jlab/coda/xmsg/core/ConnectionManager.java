JeffersonLab/xmsg-java

commit 96c29cc4f310250faa0e5311fb1af51e720e456b

ConnectionManager: use a factory to create connections

src/org/jlab/coda/xmsg/core/ConnectionManager.java
@@ -22,25 +22,28 @@
 package org.jlab.coda.xmsg.core;
 
 import org.jlab.coda.xmsg.net.xMsgConnection;
+import org.jlab.coda.xmsg.net.xMsgConnectionFactory;
 import org.jlab.coda.xmsg.net.xMsgConnectionSetup;
 import org.jlab.coda.xmsg.net.xMsgProxyAddress;
 import org.jlab.coda.xmsg.net.xMsgRegAddress;
 import org.jlab.coda.xmsg.xsys.regdis.xMsgRegDriver;
 import org.zeromq.ZContext;
-import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Socket;
 
 class ConnectionManager {
 
-    // 0MQ context object
-    private final ZContext context;
+    // Factory
+    private final xMsgConnectionFactory factory;
 
     // default connection option
     private xMsgConnectionSetup defaultConnectionOption;
 
     ConnectionManager(ZContext context) {
-        this.context = context;
+        this(new xMsgConnectionFactory(context));
+    }
 
+    ConnectionManager(xMsgConnectionFactory factory) {
+        this.factory = factory;
         // default pub/sub socket options
         defaultConnectionOption = new xMsgConnectionSetup() {
 
@@ -53,9 +56,6 @@ class ConnectionManager {
             @Override
             public void postConnection() { }
         };
-
-        // fix default linger
-        this.context.setLinger(-1);
     }
 
     xMsgConnection getProxyConnection(xMsgProxyAddress address) {
@@ -64,36 +64,19 @@ class ConnectionManager {
 
     xMsgConnection getProxyConnection(xMsgProxyAddress address,
                                       xMsgConnectionSetup setup) {
-        Socket pubSock = context.createSocket(ZMQ.PUB);
-        Socket subSock = context.createSocket(ZMQ.SUB);
-        setup.preConnection(pubSock);
-        setup.preConnection(subSock);
-
-        int pubPort = address.port();
-        int subPort = pubPort + 1;
-        pubSock.connect("tcp://" + address.host() + ":" + pubPort);
-        subSock.connect("tcp://" + address.host() + ":" + subPort);
-        setup.postConnection();
-
-        xMsgConnection connection = new xMsgConnection();
-        connection.setAddress(address);
-        connection.setPubSock(pubSock);
-        connection.setSubSock(subSock);
-
-        return connection;
+        return factory.createProxyConnection(address, setup);
     }
 
     void releaseProxyConnection(xMsgConnection connection) {
-        context.destroySocket(connection.getPubSock());
-        context.destroySocket(connection.getSubSock());
+        factory.destroyProxyConnection(connection);
     }
 
     xMsgRegDriver getRegistrarConnection(xMsgRegAddress address) {
-        return new xMsgRegDriver(context, address);
+        return factory.createRegistrarConnection(address);
     }
 
     void releaseRegistrarConnection(xMsgRegDriver connection) {
-        connection.destroy();
+        factory.destroyRegistrarConnection(connection);
     }
 
     void setDefaultConnectionSetup(xMsgConnectionSetup setup) {
@@ -101,10 +84,10 @@ class ConnectionManager {
     }
 
     void setLinger(int linger) {
-        context.setLinger(linger);
+        factory.setLinger(linger);
     }
 
     void destroy() {
-        context.destroy();
+        factory.destroy();
     }
 }

src/org/jlab/coda/xmsg/net/xMsgConnectionFactory.java
@@ -1,0 +1,62 @@
+package org.jlab.coda.xmsg.net;
+
+import org.jlab.coda.xmsg.xsys.regdis.xMsgRegDriver;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Socket;
+
+public class xMsgConnectionFactory {
+
+    private ZContext context;
+
+    public xMsgConnectionFactory(ZContext context) {
+        this.context = context;
+
+        // fix default linger
+        this.context.setLinger(-1);
+    }
+
+    public xMsgConnection createProxyConnection(xMsgProxyAddress address,
+                                                xMsgConnectionSetup setup) {
+        Socket pubSock = context.createSocket(ZMQ.PUB);
+        Socket subSock = context.createSocket(ZMQ.SUB);
+        setup.preConnection(pubSock);
+        setup.preConnection(subSock);
+
+        int pubPort = address.port();
+        int subPort = pubPort + 1;
+        pubSock.connect("tcp://" + address.host() + ":" + pubPort);
+        subSock.connect("tcp://" + address.host() + ":" + subPort);
+        setup.postConnection();
+
+        xMsgConnection connection = new xMsgConnection();
+        connection.setAddress(address);
+        connection.setPubSock(pubSock);
+        connection.setSubSock(subSock);
+
+        return connection;
+    }
+
+    public xMsgRegDriver createRegistrarConnection(xMsgRegAddress address) {
+        return new xMsgRegDriver(context, address);
+    }
+
+    public void destroyProxyConnection(xMsgConnection connection) {
+        context.destroySocket(connection.getPubSock());
+        context.destroySocket(connection.getSubSock());
+    }
+
+    public void destroyRegistrarConnection(xMsgRegDriver connection) {
+        connection.destroy();
+    }
+
+    public void setLinger(int linger) {
+        context.setLinger(linger);
+    }
+
+    public void destroy() {
+        if (!context.isMain()) {
+            context.destroy();
+        }
+    }
+}

commit 782de84eb97574d058f42dbffd0fa8e364fc37d9

perf: workaround for linger regression
96c29cc4f310250faa0e5311fb1af51e720e456b introduced a connection factory
which is not calling context.destroy() anymore. That prevented the
ZMQ context of RemoteThroughput to send all the messages on queue before
ending, and therefore LocalThroughput never finishes.

As a workaround, ensure calling context.destroy(), which does use the
linger option to wait until all messages are sent before destroying the
context.

A better solution is needed, though. Destroying an actor requires
careful thinking to ensure only sockets related to the actor are
destroyed and no message is lost.

src/org/jlab/coda/xmsg/core/ConnectionManager.java
@@ -113,6 +113,7 @@ class ConnectionManager {
     void destroy() {
         proxyConnections.destroyAll(c -> factory.destroyProxyConnection(c));
         registrarConnections.destroyAll(c -> factory.destroyRegistrarConnection(c));
+        factory.destroy();
     }
 
 

src/org/jlab/coda/xmsg/net/xMsgConnectionFactory.java
@@ -58,8 +58,6 @@ public class xMsgConnectionFactory {
     }
 
     public void destroy() {
-        if (!context.isMain()) {
-            context.destroy();
-        }
+        context.destroy();
     }
 }