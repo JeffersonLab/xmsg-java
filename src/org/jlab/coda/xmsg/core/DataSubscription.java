b1fb116a6248f96aa7664f9c0e678b9fec951283
Sebastián Mancilla <smancill@jlab.org>
2016-03-16 15:40:05 -0300

    connection: add poller class to poll SUB socket

--- src/org/jlab/coda/xmsg/core/DataSubscription.java
+++ src/org/jlab/coda/xmsg/core/DataSubscription.java
@@ -2,26 +2,24 @@ package org.jlab.coda.xmsg.core;
 
 import org.jlab.coda.xmsg.excp.xMsgException;
 import org.jlab.coda.xmsg.net.xMsgConnection;
-import org.zeromq.ZMQ.Poller;
-import org.zeromq.ZMQException;
+import org.jlab.coda.xmsg.net.xMsgPoller;
 import org.zeromq.ZMsg;
 
 class DataSubscription {
 
     final xMsgConnection connection;
+    final xMsgPoller poller;
     final String topic;
-    final Poller items;
 
     DataSubscription(xMsgConnection connection, xMsgTopic topic) throws xMsgException {
         this.connection = connection;
+        this.poller = new xMsgPoller(connection);
         this.topic = topic.toString();
-        this.items = new Poller(1);
 
         start();
     }
 
     private void start() throws xMsgException {
-        items.register(connection.getSubSock(), Poller.POLLIN);
         connection.subscribe(topic);
         if (!connection.checkSubscription(topic)) {
             throw new xMsgException("could not subscribe to " + topic);
@@ -30,11 +28,7 @@ class DataSubscription {
     }
 
     boolean hasMsg(int timeout) {
-        int rc = items.poll(timeout);
-        if (rc < 0) {
-            throw new ZMQException(connection.getSubSock().base().errno());
-        }
-        return items.pollin(0);
+        return poller.poll(timeout);
     }
 
     ZMsg recvMsg() {
