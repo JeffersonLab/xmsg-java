10c3a02f984c7659a0cd93902502145103aaa1d3
Sebastián Mancilla <smancill@jlab.org>
2016-03-18 16:29:50 -0300

    PublishersTest: extract common test code
    Prepare for adding publishing tests with a single actor instead of one
    actor per publisher thread, which will share most of the code.

--- test/org/jlab/coda/xmsg/core/PublishersTest.java
+++ test/org/jlab/coda/xmsg/core/PublishersTest.java
@@ -244,4 +244,178 @@ public class PublishersTest {
         assertThat(check.counter.get(), is(Check.N));
         assertThat(check.sum.get(), is(Check.SUM_N));
     }
+
+
+    private abstract static class TestRunner implements AutoCloseable {
+
+        final ProxyThread proxyThread = new ProxyThread();
+        final String rawTopic = "test_topic";
+        final xMsg pubActor;
+
+        TestRunner(boolean singlePubActor) {
+            this.pubActor = singlePubActor ? new xMsg("test_publisher") : null;
+            this.proxyThread.start();
+        }
+
+        abstract void receive(xMsg actor, xMsgMessage msg, Check check) throws Exception;
+
+        abstract void publish(xMsg actor, xMsgMessage msg, Check check) throws Exception;
+
+        void run(int totalMessages, int numPublishers) throws Exception {
+
+            final Check check = new Check(totalMessages);
+            final CountDownLatch subReady = new CountDownLatch(1);
+
+            Thread subThread = xMsgUtil.newThread("sub-thread", () -> {
+                try {
+                    xMsg actor = new xMsg("test_subscriber");
+                    xMsgConnection connection = actor.createConnection();
+                    xMsgTopic topic = xMsgTopic.wrap(rawTopic);
+                    xMsgSubscription sub = actor.subscribe(connection, topic, msg -> {
+                        try {
+                            receive(actor, msg, check);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    subReady.countDown();
+                    wait(check);
+                    actor.unsubscribe(sub);
+                } catch (xMsgException e) {
+                    e.printStackTrace();
+                }
+            });
+            subThread.start();
+            subReady.await();
+
+            final int numMessages = check.n / numPublishers;
+            List<Thread> publishers = new ArrayList<>(numPublishers);
+
+            for (int i = 0; i < numPublishers; i++) {
+                final int start = i * numMessages;
+                final int end = start + numMessages;
+
+                Thread pubThread = xMsgUtil.newThread("pub-" + start, () -> {
+                    try {
+                        xMsg actor = pubActor;
+                        if (actor == null) {
+                            actor = new xMsg("test_publisher_" + start);
+                        }
+                        xMsgTopic topic = xMsgTopic.build(rawTopic, Integer.toString(start));
+                        for (int j = start; j < end; j++) {
+                            xMsgMessage msg = xMsgMessage.createFrom(topic, j);
+                            publish(actor, msg, check);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                publishers.add(pubThread);
+                pubThread.start();
+            }
+
+            subThread.join();
+            for (Thread pubThread : publishers) {
+                pubThread.join();
+            }
+
+            assertThat(check.counter.get(), is(check.n));
+            assertThat(check.sum.get(), is(check.total));
+        }
+
+        private void wait(Check check) {
+            int counter = 0;
+            while (check.counter.get() < check.n && counter < 10000) {
+                xMsgUtil.sleep(100);
+                counter += 100;
+            }
+        }
+
+        @Override
+        public void close() {
+            try {
+                proxyThread.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static class AsyncRunner extends TestRunner {
+
+        AsyncRunner(boolean singlePubActor) {
+            super(singlePubActor);
+        }
+
+        @Override
+        void receive(xMsg actor, xMsgMessage msg, Check check) throws Exception {
+            int i = xMsgMessage.parseData(msg, Integer.class);
+            check.increment(i);
+        }
+
+        @Override
+        void publish(xMsg actor, xMsgMessage msg, Check check) throws Exception {
+            xMsgConnection connection = actor.getConnection();
+            try {
+                actor.publish(connection, msg);
+            } finally {
+                actor.releaseConnection(connection);
+            }
+        }
+    }
+
+
+    private static class SyncRunner extends TestRunner {
+
+        SyncRunner(boolean singlePubActor) {
+            super(singlePubActor);
+        }
+
+        @Override
+        void receive(xMsg actor, xMsgMessage msg, Check check) throws Exception {
+            xMsgConnection connection = actor.getConnection();
+            try {
+                xMsgMessage res = xMsgMessage.createResponse(msg);
+                actor.publish(connection, res);
+            } finally {
+                actor.releaseConnection(connection);
+            }
+        }
+
+        @Override
+        void publish(xMsg actor, xMsgMessage msg, Check check) throws Exception {
+            xMsgConnection connection = actor.getConnection();
+            try {
+                xMsgMessage res = actor.syncPublish(connection, msg, 1000);
+                Integer r = xMsgMessage.parseData(res, Integer.class);
+                check.increment(r);
+            } finally {
+                actor.releaseConnection(connection);
+            }
+        }
+    }
+
+
+    private static class Check {
+        final int n;
+        final long total;
+
+        AtomicInteger counter = new AtomicInteger();
+        AtomicLong sum = new AtomicLong();
+
+        Check(int n) {
+            long sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += i;
+            }
+            this.n = n;
+            this.total = sum;
+        }
+
+        void increment(int i) {
+            counter.incrementAndGet();
+            sum.addAndGet(i);
+        }
+    }
 }
