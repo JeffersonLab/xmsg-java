3ce4f8a9630fe2aec92df27d413f287b7ee64458
Sebastián Mancilla <smancill@jlab.org>
2016-05-18 13:35:45 -0400

    code analysis: make field final

--- src/org/jlab/coda/xmsg/core/ConnectionManager.java
+++ src/org/jlab/coda/xmsg/core/ConnectionManager.java
@@ -107,7 +107,7 @@ class ConnectionManager {
 
 
     static class ConnectionPool<A, C> {
-        private Map<A, Queue<C>> connections = new ConcurrentHashMap<>();
+        private final Map<A, Queue<C>> connections = new ConcurrentHashMap<>();
 
         public C getConnection(A address) {
             Queue<C> cache = connections.get(address);
